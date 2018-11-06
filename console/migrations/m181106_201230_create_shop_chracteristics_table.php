9312525e3722f2f480fa879c7eec91dc0f0656cc
Dmytro Orikhovskyi <dimaoag@gmail.com>
2018-11-06 22:30:50 +0200

    Characteristics

--- console/migrations/m181106_201230_create_shop_chracteristics_table.php
+++ console/migrations/m181106_201230_create_shop_chracteristics_table.php
@@ -7,21 +7,23 @@ use yii\db\Migration;
  */
 class m181106_201230_create_shop_chracteristics_table extends Migration
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function safeUp()
+    public function up()
     {
-        $this->createTable('shop_chracteristics', [
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+
+        $this->createTable('{{%shop_characteristics}}', [
             'id' => $this->primaryKey(),
-        ]);
+            'name' => $this->string()->notNull(),
+            'type' => $this->string(16)->notNull(),
+            'required' => $this->boolean()->notNull(),
+            'default' => $this->string(),
+            'variants_json' => 'JSON NOT NULL',
+            'sort' => $this->integer()->notNull(),
+        ], $tableOptions);
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function safeDown()
+    public function down()
     {
-        $this->dropTable('shop_chracteristics');
+        $this->dropTable('{{%shop_characteristics}}');
     }
 }

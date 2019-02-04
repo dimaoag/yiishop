d19badfb57f823e33802f23e568c30d7dc4c5daf
Dmytro Orikhovskyi <dimaoag@gmail.com>
2019-02-04 23:43:04 +0200

    supervisor and async dispatcher 2.0

--- frontend/views/layouts/catalog.php
+++ frontend/views/layouts/catalog.php
@@ -4,6 +4,7 @@
 /* @var $content string */
 
 use frontend\widgets\shop\CategoriesWidget;
+use yii\caching\TagDependency;
 
 ?>
 <?php $this->beginContent('@frontend/views/layouts/main.php') ?>
@@ -13,6 +14,14 @@ use frontend\widgets\shop\CategoriesWidget;
         <?= CategoriesWidget::widget([
             'active' => $this->params['active_category'] ?? null, // if (!isset($this->params['active_category'])) return null;
         ]) ?>
+<!--        --><?php //if (!$this->beginCache(
+//                ['widget-categories', $this->params['active_category'] ? $this->params['active_category']->id : null],
+//                ['dependency' => new TagDependency(['tags' => ['categories']])]
+//        )): ?>
+<!--            --><?php //= CategoriesWidget::widget([
+//                'active' => $this->params['active_category'] ?? null, // if (!isset($this->params['active_category'])) return null;
+//            ]) ?>
+<!--        --><?php //$this->endCache(); endif; ?>
     </aside>
     <div id="content" class="col-sm-9">
         <?= $content ?>
